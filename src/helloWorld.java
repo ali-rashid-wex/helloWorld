91fb4335568fb9c6ac111740e1043b1312b42339
ali-rashid-wex <ali.rashid@wexinc.com>
2021-10-22 13:04:42 +0100

    created hello world

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,6 +1,9 @@
 public class helloWorld {
 
     public static void main(String[] args){
+        System.out.println("Hello, Ali");
 
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
     }
 }
