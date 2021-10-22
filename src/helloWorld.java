11aed8c33765802c7f0b3603f949fc0c9428dc38
ali-rashid-wex <ali.rashid@wexinc.com>
2021-10-22 13:07:51 +0100

    added my first number

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -3,7 +3,7 @@ public class helloWorld {
     public static void main(String[] args){
         System.out.println("Hello, Ali");
 
-        int myFirstNumber = 5;
+        int myFirstNumber = (10 + 5) + (2 * 10);
         System.out.println(myFirstNumber);
     }
 }
