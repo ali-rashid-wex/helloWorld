b96d457f3b2f77d8085ac77c5ff48ebcf7f49b93
ali-rashid-wex <ali.rashid@wexinc.com>
2021-11-11 16:45:11 +0000

    completed challenges

--- src/sectionFourCodingExercise.java
+++ src/sectionFourCodingExercise.java
@@ -27,7 +27,9 @@ public class sectionFourCodingExercise {
 
         //System.out.println(isLeapYear(1600));
 
-        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        //System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+
+        System.out.println(hasTeen(122, 55, 32));
     }
 
     public static long toMilesPerHour(double kilometersPerHour){
@@ -74,6 +76,35 @@ public class sectionFourCodingExercise {
         if (strNum1.equals(strNum2)){
             return true;
         }
+        return false;
+    }
+
+    public static boolean hasEqualSum(int num1, int num2, int num3){
+        if ((num1 + num2) == num3){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean hasTeen(int num1, int num2, int num3){
+        if ((num1 >= 13) && (num1<=19)){
+            return true;
+        }
+        if ((num2 >= 13) && (num2<=19)){
+            return true;
+        }
+        if ((num3 >= 13) && (num3<=19)){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isTeen(int num1){
+        if ((num1 >= 13) && (num1 <= 19)){
+            return true;
+        }
+
         return false;
     }
 }
