207da682f363e41f57acfba81f379e791ea7a491
ali-rashid-wex <ali.rashid@wexinc.com>
2021-11-16 10:00:45 +0000

    switch statement

--- src/sectionFive.java
+++ src/sectionFive.java
@@ -1,7 +1,22 @@
 public class sectionFive {
 
     public static void main(){
-        
+        int value = 2;
+
+        swtichStatement(value);
     }
 
+    public static void swtichStatement(int val){
+        switch(val){
+            case(1):
+                System.out.println("Value was 1");
+                break;
+            case(2):
+                System.out.println("Value was 2");
+                break;
+            case(3):
+                System.out.println("Value was 3");
+                break;
+        }
+    }
 }
